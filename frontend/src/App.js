685b71329eb6406f043e05711167874eaac4745c
Colin <colin.reitman87@gmail.com>
2021-04-18 15:10:27 -0400

    frontend infrastructure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreateCarrier from './components/CreateCarrier';
+import ShowCarrierList from './components/ShowCarrierList';
+import ShowCarrierDetails from './components/ShowCarrierDetails';
+import UpdateCarrierInfo from './components/UpdateCarrierInfo';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowCarrierList} />
+          <Route path='/create-carrier' component={CreateCarrier} />
+          <Route path='/edit-carrier/:id' component={UpdateCarrierInfo} />
+          <Route path='/show-carrier/:id' component={ShowCarrierDetails} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
